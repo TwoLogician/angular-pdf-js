884077ac6603c2fb27967185670f5033d1bf1b3a
The Amazing Developer-Man <Two.Logician@icloud.com>
2019-03-19 18:15:54 +0700

    :tada:

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser"
+import { NgModule } from "@angular/core"
+import { NgxExtendedPdfViewerModule } from "ngx-extended-pdf-viewer"
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppRoutingModule } from "./app-routing.module"
+import { AppComponent } from "./app.component"
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRoutingModule,
     BrowserModule,
-    AppRoutingModule
+    NgxExtendedPdfViewerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
